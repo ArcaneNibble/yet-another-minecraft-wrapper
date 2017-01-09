385c71aad35b3f72fdc846b9ee5ae03a75232df6
Robert Ou <rqou@robertou.com>
2017-01-08 16:10:39 -0800

    Create asyncio event loop

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
+import asyncio
+import bottom
 import json
 import sys
+
+class MinecraftServerWrapper:
+    _config = None
+    _loop = None
+
+    def __init__(self, config, loop):
+        _config = config
+        _loop = loop
+
+    # Actual work starts here
+    async def start_wrapper(self):
+        print("Start!")
 
 def main():
     if len(sys.argv) < 2:
@@ -12,6 +26,12 @@ def main():
     with open(sys.argv[1], 'r') as f:
         config = json.load(f)
     print(config)
+
+    # Start event loop
+    loop = asyncio.get_event_loop()
+    serv = MinecraftServerWrapper(config, loop)
+    loop.create_task(serv.start_wrapper())
+    loop.run_forever()
 
 if __name__ == '__main__':
     main()
