70752d1b40d065f8ed0b531f908474f1eb4a5d5d
Robert Ou <rqou@robertou.com>
2017-01-08 16:04:37 -0800

    Load config file

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
+import json
 import sys
 
 def main():
     if len(sys.argv) < 2:
         print("Usage: {} config.json".format(sys.argv[0]))
         return
+
+    # Load config
+    with open(sys.argv[1], 'r') as f:
+        config = json.load(f)
+    print(config)
 
 if __name__ == '__main__':
     main()
