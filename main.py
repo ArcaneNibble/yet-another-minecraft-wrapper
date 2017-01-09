146d2c9ba6d0ef507925be7371b05277cb15969e
Robert Ou <rqou@robertou.com>
2017-01-08 16:00:57 -0800

    Add skeleton

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: {} config.json".format(sys.argv[0]))
+        return
+
+if __name__ == '__main__':
+    main()
