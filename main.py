d66eca5aa497af4539b1806acd2bfdaa1387aafa
Robert Ou <rqou@robertou.com>
2017-01-09 09:13:29 -0800

    Fix UTF-8

--- main.py
+++ main.py
@@ -53,6 +53,15 @@ IRC_TO_MC_NAME_LUT = [
     "gray"
 ]
 
+CONTROL_CODES = ('\x00\x01\x02\x03\x04\x05\x06\x07'
+                 '\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F'
+                 '\x10\x11\x12\x13\x14\x15\x16\x17'
+                 '\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F'
+                 '\x80\x81\x82\x83\x84\x85\x86\x87'
+                 '\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F'
+                 '\x90\x91\x92\x93\x94\x95\x96\x97'
+                 '\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F')
+
 
 class MinecraftServerWrapper:
     _config = None
@@ -220,7 +229,7 @@ class MinecraftServerWrapper:
             i = 0
             while i < len(message):
                 c = message[i]
-                if c in string.printable:
+                if c not in CONTROL_CODES:
                     fragment += c
 
                 elif c == '\x02':
@@ -280,7 +289,7 @@ class MinecraftServerWrapper:
             i = 0
             while i < len(message):
                 c = message[i]
-                if c in string.printable:
+                if c not in CONTROL_CODES:
                     colored_msg += c
                 elif c == '\x03':
                     # Color
